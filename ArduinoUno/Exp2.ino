712c1a31fea81d3bae9b6a3282c90df78f9f652e
Bruna <verronebruna@gmail.com>
2023-09-03 15:16:17 -0300

    [ARDUINO-UNO] add a Experiment 2

---
+++ ArduinoUno/Exp2.ino
@@ -0,0 +1,22 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(11, HIGH);
+  delay(1000);
+  digitalWrite(11, LOW);
+  digitalWrite(12, HIGH);
+  delay(1000);
+  digitalWrite(12, LOW);
+  digitalWrite(13, HIGH);
+  delay(1000);
+  digitalWrite(13, LOW);
+  delay(1000);
+}
