f8ad94535c4f8be5730021b875fcb8342b3123cf
Bruna <verronebruna@gmail.com>
2023-09-03 15:15:46 -0300

    [ARDUINO-UNO] add a Experiment 1

---
+++ ArduinoUno/Exp1.ino
@@ -0,0 +1,15 @@
+// C++ code
+//
+void setup()
+{
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(13, true);
+  digitalWrite(12, true);
+  digitalWrite(11, true);
+}
