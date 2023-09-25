0cb69b45a8cc2433b86113bb87b1afcb81b1d085
Bruna <verronebruna@gmail.com>
2023-09-25 15:12:50 -0300

    [Update] rewrite the exercise 1.2

--- Algoritmos e Programação/lista de exercicios/1-2.c
+++ Algoritmos e Programação/lista de exercicios/1-2.c
@@ -3,11 +3,9 @@
 int main(){
     //Receber dois valores, calcular sua média aritmética e exibir o resultado.
 
-    int num1, num2, media;
-    printf("Insira o primeiro número: ", num1);
-    scanf("%i", &num1);
-    printf("Insira o segundo número: ", num2);
-    scanf("%i", &num2);
-    media = (num1 + num2) / 2;
-    printf("A média aritmética de %i e %i é %i", num1, num2, media);
+    int numero1, numero2, media;
+    printf("Insira o primeiro e o segundo número: ", numero1, numero2);
+    scanf("%i %i", &numero1, &numero2);
+    media = (numero1 + numero2) / 2;
+    printf("A média aritmética de %i e %i é %i", numero1, numero2, media);
 }
