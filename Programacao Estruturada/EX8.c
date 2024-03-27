d01432e7ed3670dc98ff3defc2cb30cdc8d0e13d
Bruna Verrone <163475365+verronebrunaa@users.noreply.github.com>
2024-03-27 20:30:08 -0300

    update Ex8

--- Programacao Estruturada/EX8.c
+++ Programacao Estruturada/EX8.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-void aloca(char **p, int tam);
+void aloca(char **p, int tam); 
+void maiuscula(char *p, int tam); 
+int busca_nome(char *p, int tam, char letra, char **pl);
 
 main(){
 
-char *ptr=NULL, aux[20], op;
-int cont=0, tam;
+char *ptr=NULL, aux[20], op, letra, *pletra=NULL;
+int cont=0, tam=0, cnome;
 
 do{
     printf("\n Digite o nome: ");
@@ -15,10 +17,54 @@ do{
     fflush(stdin);
     tam=strlen(aux)+1; //por conta do '\0'
     aloca(&ptr, tam); //ponteiro > ponteiro
-}while
+    if(cont==0)
+    strcpy(ptr, aux)
+    else{
+        strcat(ptr, "\n")
+        strcat
+    }
+    strcpy(ptr, aux);
+    cont++;
+    printf("Deseja continuar <S/N>:");
+    scanf("%c", &op);
+    fflush(stdin);
+}while(op!='n'&&op!='N');
 
+printf("\n Foram alocados %i nomes: \n%s\n\n", cont, ptr);
+printf("\n Digite a letra a ser procurada: ");
+fflush(stdin);
+maiuscula(&letra,1);
+cnome=busca_nome(ptr, tam, letra, &pletra);
+system("pause");
 }
+
 void aloca(char **p, int tam){
     if((*p=(char*)realloc(*p, tam*sizeof(char)))==NULL) 
     exit(1);
+}
+
+void maiuscula(char *p, int tam){
+    int i;
+    for(i=0;i<tam; i++,p++)
+    if(*p>='a'&&*p<='z')
+    *p-=32;
+}
+
+int busca_nome(char *´, int tam, char letra, char **pl){
+int i, cont=0 qnome=0;
+
+for(i=0; i<tam;i++)
+    if((i==0 &&*(p+i)==letra)||(*(p+i)=='\n'&&*(p+i+1)==letra)){
+        qnome++;
+        do{
+            aloca(pl, cont+1);
+            *(*pl+cont)=*(p+i);
+            cont++;
+            i++;
+        }while(*(p=i)!='\n');
+        i--; //para voltar no \n
+    }
+     aloca(pl, cont+1);
+     *(*pl+cont)='\0';
+     return qnome;
 }
